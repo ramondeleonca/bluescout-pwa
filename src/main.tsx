532254cab312d5ca0293e964e8b1e0556e1f2fd6
ramondeleonca <ramondeleonca@gmail.com>
2023-08-15 14:07:50 -0600

    moved styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import "./global.scss"
+import "./styles/global.scss"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
