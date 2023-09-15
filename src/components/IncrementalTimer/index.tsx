d53a30b837446c5fbc72ec1adb6196c3a1a83747
ramondeleonca <ramondeleonca@gmail.com>
2023-09-15 14:26:16 -0600

    Update

--- src/components/IncrementalTimer/index.tsx
+++ src/components/IncrementalTimer/index.tsx
@@ -36,7 +36,7 @@ const IncrementalTimer = ({ time, setTime, running, setRunning, name }: Props) =
         <div className='flex items-end'>
             <div className='w-20'>
                 {/* <Button onClick={() => setRunning(val => !val)} variant='outlined' fullWidth>{!running && time != 0 ? "Resume" : running ? "Stop" : "Start"}</Button> */}
-                <Button onClick={() => setRunning(val => !val)} variant='outlined' fullWidth>{running ? "Stop" : "Start"}</Button>
+                {/* <Button onClick={() => setRunning(val => !val)} variant='outlined' fullWidth>{running ? "Stop" : "Start"}</Button> */}
             </div>
             <div className='w-24'>
                 <TextField name={name} type='number' value={(time / 1000).toFixed(3)} onChange={ev => setTime(val => parseNum(ev.target.value) ?? val)} variant='standard' placeholder='0' fullWidth></TextField>
